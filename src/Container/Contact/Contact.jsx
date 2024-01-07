d74f6da7dbe2b91564d336c5c6cb43ed01b7d392
Nicpaul Gacha <nicpaulgacha@gmail.com>
2024-01-07 23:22:47 +0100

    Gym

--- src/Container/Contact/Contact.jsx
+++ src/Container/Contact/Contact.jsx
@@ -1,56 +1,79 @@
-import React from 'react'
-import './contact.css'
-import {BsFacebook} from 'react-icons/bs'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiFillInstagram} from 'react-icons/ai'
-import {FaPinterestP} from 'react-icons/fa'
-import {AiFillYoutube} from 'react-icons/ai'
+import React from "react";
+import "./contact.css";
+import { BsFacebook } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaPinterestP } from "react-icons/fa";
+import { AiFillYoutube } from "react-icons/ai";
 
 const Contact = () => {
   return (
-    <div className='container_contact-all'>
-      <div className='container_contact-wrap'>
-        <div className='container_contact-info1'>
-          <div className='container_contact-header'>
-          <h2>GYM</h2>
+    <div className="container_contact-all" id="Contact">
+      <div className="container_contact-wrap">
+        <div className="container_contact-info1">
+          <div className="container_contact-header">
+            <h2>GYM</h2>
           </div>
           <p>Kruszwicka 1, 85-213 Bydgoszcz, Poland</p>
           <p>+48 369 518 636</p>
           <p>contact@carpenter.com</p>
-          <div className='container_contact-icons'>
-           <li>
-            <ul><a href="#1"><BsFacebook/></a></ul>
-            <ul><a href="#2"><AiFillInstagram/></a></ul>
-            <ul><a href="#3"><AiOutlineTwitter/></a></ul>
-            <ul><a href="#4"><FaPinterestP/></a></ul>
-            <ul><a href="#5"><AiFillYoutube/></a></ul>
-           </li>
+          <div className="container_contact-icons">
+            <li>
+              <ul>
+                <a href="#1">
+                  <BsFacebook />
+                </a>
+              </ul>
+              <ul>
+                <a href="#2">
+                  <AiFillInstagram />
+                </a>
+              </ul>
+              <ul>
+                <a href="#3">
+                  <AiOutlineTwitter />
+                </a>
+              </ul>
+              <ul>
+                <a href="#4">
+                  <FaPinterestP />
+                </a>
+              </ul>
+              <ul>
+                <a href="#5">
+                  <AiFillYoutube />
+                </a>
+              </ul>
+            </li>
           </div>
         </div>
-        <div  className='container_contact-info2'> 
-          <div  className='container_contact-header'>
-          <h2>USEFUL</h2>
+        <div className="container_contact-info2">
+          <div className="container_contact-header">
+            <h2>USEFUL</h2>
           </div>
           <p>Pricing</p>
           <p>About</p>
           <p>Gallery</p>
           <p>Contact</p>
         </div>
-        <div className='container_contact-info3'>
-          <div  className='container_contact-header'>
+        <div className="container_contact-info3">
+          <div className="container_contact-header">
             <h2>Subscribe</h2>
           </div>
-          <div className='container_contact-form'>
+          <div className="container_contact-form">
             <form action="">
-              <input type="text" name="" id="" placeholder='Enter your email'/>
-              <button className='container_contact-button'>JOIN US</button>
+              <input type="text" name="" id="" placeholder="Enter your email" />
+              <button className="container_contact-button">JOIN US</button>
             </form>
-            <p>Esteem spirit temper too say adieus who direct esteem esteems luckily.</p>
+            <p>
+              Esteem spirit temper too say adieus who direct esteem esteems
+              luckily.
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Container/Gallery/Gallery.jsx
+++ src/Container/Gallery/Gallery.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import './gallery.css'
-import john from '../../Assets/john-fornander-TAZoUmDqzXk-unsplash.jpg'
-import fitness from '../../Assets/exercise-4607682_1920.jpg'
-import abs from '../../Assets/abs-1850926_1920.jpg'
-import anastase from '../../Assets/anastase-maragos-4dlhin0ghOk-unsplash.jpg'
-import pexels from '../../Assets/pexels-leon-ardho-1552242.jpg'
-import photo from '../../Assets/photo-1605296867304-46d5465a13f1.jpeg'
+import React from "react";
+import "./gallery.css";
+import john from "../../Assets/john-fornander-TAZoUmDqzXk-unsplash.jpg";
+import fitness from "../../Assets/exercise-4607682_1920.jpg";
+import abs from "../../Assets/abs-1850926_1920.jpg";
+import anastase from "../../Assets/anastase-maragos-4dlhin0ghOk-unsplash.jpg";
+import pexels from "../../Assets/pexels-leon-ardho-1552242.jpg";
+import photo from "../../Assets/photo-1605296867304-46d5465a13f1.jpeg";
 
 const Gallery = () => {
   return (
-      <div className='container_gallery-images'>
-        <div className='img1'>
-          <img src={john} alt="#" />
-        </div>
-        <div className='img2'>
-          <img src={fitness} alt="#" />
-        </div>
-       <div className='img3'>
-         <img src={abs} alt="#" />
-       </div>
-        <div className='img4'> 
-          <img src={anastase} alt="#" />
-        </div>
-       <div className='img5'>
-         <img src={pexels} alt="#" />
-       </div>
-       <div className='img6'>
-         <img src={photo} alt="#" /> 
-       </div>
+    <div className="container_gallery-images" id="Gallery">
+      <div className="img1">
+        <img src={john} alt="#" />
       </div>
-  )
-}
+      <div className="img2">
+        <img src={fitness} alt="#" />
+      </div>
+      <div className="img3">
+        <img src={abs} alt="#" />
+      </div>
+      <div className="img4">
+        <img src={anastase} alt="#" />
+      </div>
+      <div className="img5">
+        <img src={pexels} alt="#" />
+      </div>
+      <div className="img6">
+        <img src={photo} alt="#" />
+      </div>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
